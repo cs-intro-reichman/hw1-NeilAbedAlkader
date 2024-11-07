e7283804d51353ba98543161c936fb4e5338f99e
MohamadAbedAlkader <mam10102004@gmail.com>
2024-11-07 10:38:25 +0200

    a

--- Bill3.java
+++ Bill3.java
@@ -2,8 +2,10 @@
 public class Bill3 {
 	public static void main(String[] args) {
 		// To get you started, here is the first line in the program:
-	    String name1 = args[0];
+		int bill = Integer.parseInt(args[0]);
+	    String name1 = args[1];
+		String name2 = args[2];
+		String name3 = args[3];
 	    // Replace this comment with the rest of your code
-		// babababab
-	} 
+	}
 }
