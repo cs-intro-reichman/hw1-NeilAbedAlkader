133bca38e4722d34857bafeeafcc717035e2a247
NeilAbedAlkader <mam10102004@gmail.com>
2024-11-12 23:54:17 +0200

    Update FVCalc.java

--- FVCalc.java
+++ FVCalc.java
@@ -7,7 +7,7 @@ public class FVCalc {
 		int years = Integer.parseInt(args[2]);
 		double a =intrest/100;
 		double futureValue = saving * Math.pow(1 + a, years);
-		System.out.println("After " + years + "years, $" + saving + " saved at " + intrest + "% will yield $" + (int)futureValue);
+		System.out.println("After " + years + " years, $" + saving + " saved at " + intrest + "% will yield $" + (int)futureValue);
 
 
 	}
